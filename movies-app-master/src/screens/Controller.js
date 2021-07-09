a8d4532445411c25268619005f348aff080ae624
sg185293 <sg185293@ncr.com>
2021-07-09 15:58:36 +0530

    Home page with Header details

---
+++ movies-app-master/src/screens/Controller.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Home from '../screens/home/Home';
+import Details from '../screens/details/Details';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BookShow from '../screens/bookshow/BookShow';
+import Confirmation from '../screens/confirmation/Confirmation';
+
+class Controller extends Component {
+
+  constructor() {
+    super();
+    this.baseUrl = "http://localhost:8085/api/v1/";
+  }
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+          
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default Controller;
